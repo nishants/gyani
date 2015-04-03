ca53e7680abad2dada02c819745ca7c9c01b123a
nishant <nishant.singh87@gmail.com>
2015-04-03 20:20:48 +0530

    creating api of adding new pages

--- indexer_service/lib/controllers/index.rb
+++ indexer_service/lib/controllers/index.rb
@@ -1,7 +1,21 @@
 class Application < Sinatra::Base
 
   put "/pages" do
+  	params["keywords"] = create_if_not_exists(params.delete("keywords"));
     Page.new(params).save
+  end
+
+  def create_if_not_exists keywords
+  	result = [];
+  	keywords.each{ |key|
+  		keyword = Keyword.where(text: key).first;
+  		unless keyword
+  			keyword = Keyword.new({text: key}) ;
+  			keyword.save;
+  		end
+  		result.push(keyword)
+  	}
+  	return result;
   end
 
 end
--- indexer_service/spec/system/create_page_spec.rb
+++ indexer_service/spec/system/create_page_spec.rb
@@ -4,15 +4,31 @@ describe Application do
 
   before :each do
     Page.delete_all
+    Keyword.delete_all
   end
 
-  context "Sample test" do
-    it "should return ok" do
-      put "/pages", {url: "www.geeksaint.com", keywords: "Ford, Car, Review" }
-      last_response.status.should == 200
-      created_page = Page.all()[0]
-      created_page.url.should == "www.geeksaint.com"
-      created_page.keywords.should == "Ford, Car, Review"
+  context "Add a page" do
+    it "should  ok" do
+      put "/pages", {url: "www.geeksaint.com", keywords: ["Ford", "Car"] };
+      last_response.status.should == 200;
+      created_page = Page.all()[0];
+      created_page.url.should == "www.geeksaint.com";
+      Keyword.all().length.should == 2;
+
     end
+
+    it "should create a keyword, only if it doesnt already exists in the db." do
+      Keyword.new(text: "Ford").save;
+      fordKey = Keyword.all().first;
+
+      put "/pages", {url: "www.geeksaint.com", keywords: ["Ford", "Car", "Review"] };
+      last_response.status.should == 200;
+
+      fordKey.id.should == Keyword.where(text: "Ford").first.id;
+
+      Keyword.all().length.should == 3;
+    end
+
   end
+
 end
